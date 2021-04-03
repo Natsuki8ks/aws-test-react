e899befb09849b5b1ef5c1c273702592c0f4554e
Natsuki <nkondo3@gmail.com>
2021-04-03 10:14:47 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://ap-northeast-1.console.aws.amazon.com/console/home?region=ap-northeast-1"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello AWS
         </a>
       </header>
     </div>
